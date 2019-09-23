34261c4412b7e8e414debeeefb6d434651057b6f
梁柱 <liangzhu@77ircloud.com>
2019-09-24 01:09:19 +0800

    1

--- rollup.config.js
+++ rollup.config.js
@@ -22,56 +22,56 @@ const umdGlobals = {
 };
 
 export default [
-  {
-    input,
-    output: {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    external,
-    plugins: [
-      babel({
-        runtimeHelpers: true,
-        plugins: ['@babel/transform-runtime'],
-        sourceMaps: true,
-      }),
-      nodeResolve(),
-      commonjs(),
-    ],
-  },
+  // {
+  //   input,
+  //   output: {
+  //     file: pkg.main,
+  //     format: 'cjs',
+  //     sourcemap: true,
+  //   },
+  //   external,
+  //   plugins: [
+  //     babel({
+  //       runtimeHelpers: true,
+  //       plugins: ['@babel/transform-runtime'],
+  //       sourceMaps: true,
+  //     }),
+  //     nodeResolve(),
+  //     commonjs(),
+  //   ],
+  // },
 
-  {
-    input,
-    output: {
-      file: pkg.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-    external,
-    plugins: [babelConfigEsModules, nodeResolve(), commonjs()],
-  },
+  // {
+  //   input,
+  //   output: {
+  //     file: pkg.module,
+  //     format: 'esm',
+  //     sourcemap: true,
+  //   },
+  //   external,
+  //   plugins: [babelConfigEsModules, nodeResolve(), commonjs()],
+  // },
 
-  {
-    input,
-    output: {
-      file: 'dist/index-dev.umd.js',
-      format: 'umd',
-      sourcemap: true,
-      name: 'ReactWindow',
-      globals: umdGlobals,
-    },
-    external: Object.keys(umdGlobals),
-    plugins: [
-      babelConfigEsModules,
-      nodeResolve(),
-      commonjs(),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-      }),
-      terser(),
-    ],
-  },
+  // {
+  //   input,
+  //   output: {
+  //     file: 'dist/index-dev.umd.js',
+  //     format: 'umd',
+  //     sourcemap: true,
+  //     name: 'ReactWindow',
+  //     globals: umdGlobals,
+  //   },
+  //   external: Object.keys(umdGlobals),
+  //   plugins: [
+  //     babelConfigEsModules,
+  //     nodeResolve(),
+  //     commonjs(),
+  //     replace({
+  //       'process.env.NODE_ENV': JSON.stringify('development'),
+  //     }),
+  //     terser(),
+  //   ],
+  // },
 
   {
     input,
